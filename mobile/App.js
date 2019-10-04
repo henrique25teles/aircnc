0e9298fb1826dea3d30cd1e3f3b9ae1437bdebdd
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-10-04 13:17:27 -0300

    Finalizacao projeto mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Vem dar meia hora de cu !!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fffaaa',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,39 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage, StatusBar} from 'react-native';
+import logo from '../assets/logo.png';
+import SpotList from '../components/Spotlist';
 
 export default function List() {
-    return <View />;
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storageTechs => {
+            const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+        paddingBottom: StatusBar.currentHeight,
+    },
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10,
+    },
+});
